4957546696a68ead29ad20bd2db09e71136fa35c
louis baptiste <louisvis@ymail.com>
2018-06-19 06:53:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,13 @@ end
 
 
 
-
+def self.find_or_create_by_name(name)
+  if self.find_by_name(name)== name
+    return self.find_by_name(name)
+  else
+    self.create_by_name(name)
+   end
+end
 
 
 
